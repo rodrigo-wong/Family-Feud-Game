1d047d79f8eb0c16680cadc2b0008f567c6c0b1d
wongmacr <wongmacr@gmail.com>
2026-08-04 11:43:58 -0400

    add button and fix styling

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,8 +4,14 @@ import logo from "../assets/family-feud-logo.png";
 class Intro extends Component {
     render() {
         return (
-            <div c>
-                <img src={logo} alt="Logo"/>
+            <div className="relative flex flex-col items-center justify-center min-h-screen">
+                <img src={logo} alt="Logo" />
+
+                <div className="top-[calc(75%+120px)] transform -translate-y-1/2">
+                    <p className="text-center text-4xl bg-gray-900 border-2 rounded-2xl border-white p-2 text-white w-60 cursor-pointer">
+                        START GAME
+                    </p>
+                </div>
             </div>
         );
     }
